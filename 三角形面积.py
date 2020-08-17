0f42ab25f7b8e76dc963698134f413c28a683c8c
nathanyanzx <69667983+nathanyanzx@users.noreply.github.com>
2020-08-17 11:14:44 +0800

    Update 三角形面积.py

--- 三角形面积.py
+++ 三角形面积.py
@@ -1,7 +1,7 @@
 def triangle(a,b,c):
     if a+b>c and a+c>b and b+c>a:
         p=(a+b+c)/2
-        s=(p*(p-a)*(p-b)*(p-c))**0.5
+        s=(p*(p-a)*(p-b)*(p-c))**0.5 #海伦公式
         return '三角形的面积是：', s
     else:
         raise ValueError('两边之和必须大于第三边')
